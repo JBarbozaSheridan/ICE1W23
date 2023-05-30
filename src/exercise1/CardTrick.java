594c6fdd0aa3fc22bc1c21c0d0d9b65386138d67
José Barboza <barbozag@sheridancollege.ca>
2023-05-30 09:44:15 -0400

    Random function created

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,5 @@
 package exercise1;
-
+import java.util.*;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -16,14 +16,32 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
+
+            Random ran = new Random();
+            int rdm = ran.nextInt(14);
+        
+            card.setValue(rdm);
+            System.out.println(rdm);
+            
+            int rdmSuit = ran.nextInt(3);
+            
+            card.setSuit(Card.SUITS[rdmSuit]);
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-
+       
         // insert code to ask the user for Card value and suit, create their card
+        
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Insert a Card Value between 1 and 12");
+        int userCardValue = input.nextInt();
+        
+        System.out.println("Insert a Card Suite");
+        int userCardSuite = input.nextInt();
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
         //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
